9e900263521aa0363a9f9f97270ca47e19fd87c4
Ranit Dubey <ranitdubey@gmail.com>
2016-12-16 14:09:18 -0800

    added people var, html skeleton, and js skeleton

---
+++ people.js
@@ -0,0 +1,188 @@
+var cohort = [
+	{
+		first: 'Cynthia',
+		last: 'Bathgate',
+		img: 'assets/Cynthia.png'
+	},
+	{
+		first: 'Stephen',
+		last: 'Chan',
+		img: 'assets/Stephen.png'
+	},
+	{
+		first: 'Derrick',
+		last: 'Chan',
+		img: 'assets/Derrick.png'
+	},
+	{
+		first: 'Kevin',
+		last: 'Gin',
+		img: 'assets/Kevin.png' //DUP?
+	},
+	{
+		first: 'Joey',
+		last: 'Laguna',
+		img: 'assets/Joey.png'
+	},
+	{
+		first: 'Saivickna',
+		last: 'Raveendran',
+		img: 'assets/Sai.png',
+		nickname: 'Sai'
+	},
+	{
+		first: 'Jasmine',
+		last: 'Mina',
+		img: 'assets/Jasmine.png'
+		nickname: 'Jmina',
+		pronunciation: '(jay-me-nuh)'
+	},
+	{
+		first: 'Michael',
+		last: 'Tutt',
+		img: 'assets/Michael.png' //dup?
+	},
+	{
+		first: 'Fred',
+		last: 'Li',
+		img: 'assets/Fred.png'
+	},
+	{
+		first: 'James',
+		last: 'Falkoff',
+		img: 'assets/James.png'
+	},
+	{
+		first: 'Jeffrey',
+		last: 'Villarina',
+		img: 'assets/Jeffrey.png'
+	},
+	{
+		first: 'Sunnie',
+		last: 'Mao',
+		img: 'assets/Sunnie.png'
+	},
+	{
+		first: 'Jeremy',
+		last: 'Sande',
+		img: 'assets/Jeremy.png'
+	},
+	{
+		first: 'Andrew',
+		last: 'Regal',
+		img: 'assets/Andrew.png'
+	},
+	{
+		first: 'Jordan',
+		last: 'Medina',
+		img: 'assets/Jordan.png'
+	},
+	{
+		first: 'Michael',
+		last: 'Merrill',
+		img: 'assets/Michael.png' //DUP?
+	},
+	{
+		first: 'Nathan',
+		last: 'Tuong',
+		img: 'assets/Nathan.png'
+	},
+	{
+		first: 'Hansheng',
+		last: 'Zhao',
+		img: 'assets/Han.png'
+	},
+	{
+		first: 'Bianca',
+		last: 'Woo',
+		img: 'assets/Bianca.png'
+	},
+	{
+		first: 'Patrick',
+		last: 'Shen',
+		img: 'assets/Patrick.png'
+	},
+	{
+		first: 'Maurice',
+		last: 'Ochieng',
+		img: 'assets/Maurice.png'
+	},
+	{
+		first: 'Jennifer',
+		last: 'Sha',
+		img: 'assets/Jennifer.png',
+		nickname: 'Jen'
+	},
+	{
+		first: 'Anthony',
+		last: 'Pecchillo',
+		img: 'assets/Anthony.png'
+	},
+	{
+		first: 'Tony',
+		last: 'Lopes',
+		img: 'assets/Tony.png'
+	},
+	{
+		first: 'Ramsha',
+		last: 'Mushtaq',
+		img: 'assets/Ramsha.png',
+		pronunciation: 'Rumsha'
+	},
+	{
+		first: 'Richard',
+		last: 'Huber',
+		img: 'assets/Richard.png'
+	},
+	{
+		first: 'Isaac',
+		last: 'Yoon',
+		img: 'assets/Isaac.png'
+	},
+	{
+		first: 'Benson',
+		last: 'Ninh',
+		img: 'assets/Benson.png'
+	},
+	{
+		first: 'Greg',
+		last: 'Coffeng',
+		img: 'assets/Greg.png'
+	},
+	{
+		first: 'Andrew',
+		last: 'Hsu',
+		img: 'assets/Andrew.png'
+	},
+	{
+		first: 'Jimothy',
+		last: 'Zhang',
+		img: 'assets/Jimothy.png'
+	},
+	{
+		first: 'Benze',
+		last: 'Gong',
+		img: 'assets/Benze.png'
+	},
+	{
+		first: 'Ryan',
+		last: 'Gray',
+		img: 'assets/Ryan.png'
+	},
+	{
+		first: 'Nicholas',
+		last: 'Cobbett',
+		img: 'assets/Nick.png',
+		nickname: 'Nick'
+	},
+	{
+		first: 'Michelle',
+		last: 'Choi',
+		img: 'assets/Michelle.png'
+	},
+	{
+		first: 'Kevin',
+		last: 'Wong',
+		img: 'assets/Kevin-Wong.png'
+	}
+];
